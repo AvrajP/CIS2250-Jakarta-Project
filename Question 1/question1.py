3e89f9d70266b270f86e4507b314bafab8041b9a
AvrajP <avraj@uoguelph.ca>
2026-03-09 15:15:48 -0400

    Create question1.py
    added authors

---
+++ Question 1/question1.py
@@ -0,0 +1,20 @@
+'''
+question1.py
+  Author(s): Hafsa Ahmed [1396772], Marcus Le [1385741], Avraj Pannu [1376060]
+
+
+  Project: Milestone 3
+  Date of Last Update: Mar 9, 2026.
+
+  Functional Summary
+    
+     Commandline Parameters: 
+        
+
+     References:
+        * Elections Canada (2022): "Voting Results for the 43rd General Election",
+            https://www.elections.ca/content.aspx?section=res&dir=rec/eval/pes2019/vtsa&document=index&lang=e
+        * Government of Canada (2022): "43th General Election:
+            Official Voting Results", Record ID:
+            https://open.canada.ca/data/en/dataset/199e5070-2fd5-49d3-aa21-aece08964d18/resource/12df72de-795e-44c4-8e94-fa86b6708ca5
+'''
